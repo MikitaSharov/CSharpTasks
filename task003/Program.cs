ef6c2c234e545e2552fd497be2b727d0e6d2f457
Nikita Sharov <lliapik@gmail.com>
2023-08-02 11:07:01 +0300

    task003

---
+++ task003/Program.cs
@@ -0,0 +1,8 @@
+﻿// Вывести на экран два числа от пользователя одно под другим.
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Первое число: {number1}");
+Console.WriteLine($"Второе число: {number2}");
