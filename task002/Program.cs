cca10c651a201eeae2f69e14bc4aea8843a58fb4
Nikita Sharov <lliapik@gmail.com>
2023-08-02 10:59:46 +0300

    task002

---
+++ task002/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Вывести на одной строке три числа от пользователя с одним пробелом между ними.
+*/
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{number1} {number2} {number3}");
