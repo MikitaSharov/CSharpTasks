44ed7c0fa1c05321dbf6804dd768282b634ae8f3
Nikita Sharov <lliapik@gmail.com>
2023-08-02 11:36:31 +0300

    task005

---
+++ task005/Program.cs
@@ -0,0 +1,5 @@
+﻿//Дана сторона квадрата. Найти его периметр.
+
+Console.Write("Enter the side of the square: ");
+int Side_Square = Convert.ToInt32(Console.ReadLine());
+Console.Write($"P={Side_Square*4}");
