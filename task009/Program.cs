0c62047d618c7741879c9a2fe007d290a760b4f9
Nikita Sharov <lliapik@gmail.com>
2023-08-03 12:11:30 +0300

    task009_double:N3, convertToDouble

--- task009/Program.cs
+++ task009/Program.cs
@@ -6,11 +6,16 @@
 double cost_candies = new Random().NextDouble() * 10;
 double cost_cookies = new Random().NextDouble() * 5;
 double cost_apples = new Random().NextDouble() * 20;
-double candies = new Random().NextDouble();
-double cookies = new Random().NextDouble();
-double apples = new Random().NextDouble();
 
 Console.WriteLine($"cost candies = {cost_candies:N3}, cost cookies = {cost_cookies:N3}, cost apples = {cost_apples:N3}");
-Console.WriteLine($"candies = {candies:N3}, cookies = {cookies:N3}, apples = {apples:N3}");
 
-Console.WriteLine($"Amount = {cost_candies * candies + cost_cookies * cookies + cost_apples * apples:N3}");
+Console.Write("Enter candies: ");
+double candies = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter cookies: ");
+double cookies = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter apples: ");
+double apples = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine($"candies = {candies:N3}, cookies = {cookies:N3}, apples = {apples:N3}");
+
+Console.WriteLine($"Amount price: {cost_candies * candies + cost_cookies * cookies + cost_apples * apples:N3}");
