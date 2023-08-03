c50c98b1305ede5a6f80ce33a8df28b8578c44fb
Nikita Sharov <GB@gmail.com>
2023-08-03 18:34:01 +0300

    task011

---
+++ task011/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Возраст Тани — X лет, а возраст Мити — Y лет.
+Найти их средний возраст, а также определить,
+на сколько отличается возраст каждого ребенка от среднего значения. */
+
+int ageT = new Random().Next(0,50);
+int ageM = new Random().Next(0,50);
+
+Console.WriteLine($"average age of children {(float)(ageT+ageM)/2} years");
+if (ageM>ageT)
+Console.WriteLine($"children's age differs from their average by {(float)(ageM-ageT)/2} years");
+else Console.WriteLine($"children's age differs from their average by {(float)(ageT-ageM)/2} years");
