91c1b713749d39cb61239d216ebe515c671357a8
Nikita Sharov <lliapik@gmail.com>
2023-08-03 09:35:08 +0300

    task007

---
+++ task007/Program.cs
@@ -0,0 +1,6 @@
+﻿//Дана длина ребра куба. Найти объем куба и площадь его боковой поверхности.
+
+Console.WriteLine("Enter length: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"V = {length*length*length}");
+Console.WriteLine($"S = {length*length}");
