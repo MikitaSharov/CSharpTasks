854d416817257faac8b1eaa55bb795658c74612f
Nikita Sharov <lliapik@gmail.com>
2023-08-04 13:20:41 +0300

    task017 and task016_char_choice

--- task016/Program.cs
+++ task016/Program.cs
@@ -3,13 +3,36 @@
     а) b присвоить значение c, а присвоить значение b, с присвоить значение а;
     б) b присвоить значение а, с присвоить значение b, а присвоить значение с. */
 
-    int a = 1;
-    int b = 2;
-    int c = 3;
-    int temp = a;
+int a = new Random().Next(-10, 10);
+int b = new Random().Next(-10, 10);
+int c = new Random().Next(-10, 10);
 
-    Console.WriteLine($"a={a=b}, b={b=c}, c={c=temp}");
+Console.WriteLine($"a = {a}, b = {b}, c = {c}");
 
-    temp = a;
+char choice = Convert.ToChar("0");
+int number_choice = 0;
 
-    Console.WriteLine($"a={a = c}, c={c=b}, b={b=temp}");
+while (number_choice == 0)
+{
+    Console.WriteLine("Для выбора схемы нажмите на клавиатуре a или b");
+    choice = Console.ReadKey(true).KeyChar;
+    if (choice == Convert.ToChar("a")) number_choice = 1;
+    else if (choice == Convert.ToChar("b")) number_choice = 2;
+    else Console.WriteLine("Неверный ввод");
+}
+int temp = 0;
+if (number_choice == 1)
+{
+    temp = b;
+    b = c;
+    c = a;
+    a = temp;
+}
+else if (number_choice == 2)
+{
+    temp = b;
+    b = a;
+    a = c;
+    c = temp;
+}
+Console.WriteLine($"a = {a}, b = {b}, c = {c}");
---
+++ task017/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Дано вещественное число а. Пользуясь только операцией умножения, получить:
+а) a4 за две операции;
+б) a6  за три операции;
+в) a7 за четыре операции;
+г) a8  за три операции;
+д) a9 за четыре операции;
+е) a10  за четыре операции. */
+
+// double a = -10 + new Random().NextDouble() * 20;
+
+double a = 2;
+double a2=a*a;
+double a4=a2*a2;
+double a6=a2*a2*a2;
+double a7=a2*a2*a2*a;
+double a8=a4*a4;
+double a9=a4*a4*a;
+double a10=a4*a4*a2;
+
+Console.WriteLine($"a4 = {a4}");
+Console.WriteLine($"a6 = {a6}");
+Console.WriteLine($"a7 = {a7}");
+Console.WriteLine($"a8 = {a8}");
+Console.WriteLine($"a9 = {a9}");
+Console.WriteLine($"a10 = {a10}");
