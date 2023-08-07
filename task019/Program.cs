41c4a6fdd93b851129700d6465778afd28b43a40
Nikita Sharov <lliapik@gmail.com>
2023-08-07 10:47:30 +0300

    task020_XY_X+Y_X*Y

--- task019/Program.cs
+++ task019/Program.cs
@@ -1,5 +1,5 @@
 ﻿// С некоторого момента прошло 234 дня. Сколько полных недель прошло за этот период?
 
-int days = new Random().Next(0,1000);
-int weeks = days/7;
+int days = new Random().Next(0, 1000);
+int weeks = days / 7;
 Console.WriteLine($"{days} days = {weeks} weeks");
---
+++ task020/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Дано двузначное число. Найти:
+а) число десятков в нем;
+б) число единиц в нем;
+в) сумму его цифр;
+г) произведение его цифр. */
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"In {number} {number / 10} tens and {number % 10} ones");
+Console.WriteLine($"The sum of the digits of a number {number} = {number / 10 + number % 10}");
+Console.WriteLine($"The multiplication of the digits of a number {number} = {number / 10 * (number % 10)}");
