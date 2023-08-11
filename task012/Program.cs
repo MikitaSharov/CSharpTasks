b45bc8d086aa227a9204aa53e16407558f8cd14a
Nikita Sharov <lliapik@gmail.com>
2023-08-11 12:31:20 +0300

    :N0 лучше менять на (int)

--- task012/Program.cs
+++ task012/Program.cs
@@ -5,4 +5,4 @@ double v1 = new Random().NextDouble() * 300;
 double v2 = new Random().NextDouble() * 300;
 double S = new Random().NextDouble() * 3000;
 
-Console.WriteLine($"Cars will meet in {S / (v1 + v2):N0} hours {(S * 60 / (v1 + v2)) % 60:N0} minutes");
+Console.WriteLine($"Cars will meet in {(int)(S / (v1 + v2))} hours {(int)(S * 60 / (v1 + v2)) % 60} minutes");
